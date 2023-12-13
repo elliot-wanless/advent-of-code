4710f7d8cba88ac8ebb2bcf5bd82890c3247a56e
Elliot Wanless <elliotwanless@Elliots-MacBook-Pro-2.local>
2023-12-13 15:48:32 +0000

    Rrefacctor and comment

--- python/13_day.py
+++ python/13_day.py
@@ -6,35 +6,44 @@ total = 0
 
 def find_reflections(grid):
     for i in range(1, len(grid)):
-        up = grid[:i]
+        # Split the grid into two parts
+        # Up are the rows above the reflection (reversed for easier comparison)
+        up = grid[:i][::-1]
+        # Down are the rows below the reflection
         down = grid[i:]
-        up = up[::-1]
-
-        minimum_length = min(len(up), len(down))
-        differences = 0
 
         # Part 2
-        # for zipped_up, zipped_down in zip(up, down):
-        #     for char1, char2 in zip(zipped_up, zipped_down):
-        #         if(char1 != char2):
-        #             differences += 1
+        differences = 0
+        # Look at  each  character in the up/down rows
+        for zipped_up, zipped_down in zip(up, down):
+            for char1, char2 in zip(zipped_up, zipped_down):
+                # If they're different, increment differences
+                if(char1 != char2):
+                    differences += 1
 
-        # if differences == 1:
-        #     return i
-
-        # Part 1
-        up = up[:minimum_length]
-        down = down[:minimum_length]
-        
-        if up == down:
+        # We only want to change one 'smudge'
+        if differences == 1:
             return i
+
+        # # Part 1
+        # minimum_length = min(len(up), len(down))
+        # up = up[:minimum_length]
+        # down = down[:minimum_length]
+        
+        # if up == down:
+        #     return i
         
     return 0
 
 for grid in section:
+    # Rotate the grid so I can reuse my function above
     rotated_grid = list(zip(*grid))
+
+    # Get horizontal and vertical reflections
     horizontal = find_reflections(grid)
     vertical = find_reflections(rotated_grid)
-    total += (horizontal*100 + vertical)
+
+    # Add to the total
+    total += horizontal*100 + vertical
 
 print(total)
