1440de29914951f42860ffcb6cbc71f9f3a0a89d
Elliot Wanless <elliotwanless@Elliots-MacBook-Pro-2.local>
2023-12-17 18:55:41 +0000

    rename tiles to blocks

--- python/17_day.py
+++ python/17_day.py
@@ -18,7 +18,7 @@ while queue:
         print(heat_loss)
         break
 
-    # If we've visited this tile before, skip
+    # If we've visited this block before, skip
     if (r, c, r_dir, c_dir, count) in visited:
         continue
 
@@ -28,7 +28,7 @@ while queue:
     if count < 10 and (r_dir, c_dir) != (0, 0):
         new_r = r + r_dir
         new_c = c + c_dir
-        # If the new row and column aren't out of bounds, add the tile to the queue
+        # If the new row and column aren't out of bounds, add the block to the queue
         if 0 <= new_r < len(grid) and 0 <= new_c < len(grid[0]):
             extra_loss = grid[new_r][new_c]
             heappush(queue, (heat_loss + extra_loss, new_r, new_c, r_dir, c_dir, count + 1))
@@ -36,11 +36,11 @@ while queue:
     # If we have travelled 4 or more or we don't have a direction (e.g. at the start), split into 4 directions
     if count >= 4 or (r_dir, c_dir) == (0, 0):
         for new_r_dir, new_c_dir in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-            # If the new direction isn't the same or doubling back on the path, add the tile to the queue
+            # If the new direction isn't the same or doubling back on the path, add the block to the queue
             if (new_r_dir, new_c_dir) != (r_dir, c_dir) and (new_r_dir, new_c_dir) != (-r_dir, -c_dir):
                 new_r = r + new_r_dir
                 new_c = c + new_c_dir
-                # If not out of bounds, add the tile to the queue with count of 1 (as new direction started)
+                # If not out of bounds, add the block to the queue with count of 1 (as new direction started)
                 if 0 <= new_r < len(grid) and 0 <= new_c < len(grid[0]):
                     extra_loss = grid[new_r][new_c]
                     heappush(queue, (heat_loss + extra_loss, new_r, new_c, new_r_dir, new_c_dir, 1))
