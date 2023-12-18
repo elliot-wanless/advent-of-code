495b7fa23fb867fde6b552c4ca57c71a62b677d0
Elliot Wanless <elliotwanless@Elliots-MacBook-Pro-2.local>
2023-12-18 13:01:44 +0000

    rename visited_list to visited

--- python/18_day.py
+++ python/18_day.py
@@ -1,6 +1,6 @@
 input = [line.split(" ") for line in open('python/inputs.txt').read().splitlines()]
 
-visited_list = list()
+visited = list()
 length, current = 0, (0, 0)
 mapping = {"R": (0, 1), "D": (1, 0), "L": (0, -1), "U": (-1, 0)}
 
@@ -19,10 +19,10 @@ for line in input:
     # Set current to the end of the instruction
     current = (r + r_dir*n, c + c_dir*n)
     # Add the whole edge to the visited list
-    visited_list.append(current)
+    visited.append(current)
 
 # Area of a polygon = 1/2 * abs(sum(x[i]*y[i+1] - x[i+1]*y[i]))
-A = abs(sum(visited_list[i][0] * (visited_list[i - 1][1] - visited_list[(i + 1) % len(visited_list)][1]) for i in range(len(visited_list)))) // 2
+A = abs(sum(visited[i][0] * (visited[i - 1][1] - visited[(i + 1) % len(visited)][1]) for i in range(len(visited)))) // 2
 i = A - length // 2 + 1
 
 # Inner-area plus the outer loop
