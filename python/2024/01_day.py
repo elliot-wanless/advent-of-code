056974f91137940442a25d5e2e22800bd0e4b3a0
Elliot Wanless <elliotwanless@Elliots-MacBook-Pro-2.local>
2024-12-01 09:32:45 +0000

    perf improvements

--- python/2024/01_day.py
+++ python/2024/01_day.py
@@ -7,29 +7,27 @@ for line in input:
     list1.append(left)
     list2.append(right)
 
-
 ### Part 1 ###
-total = 0
 list1.sort()
 list2.sort()
-
 total = sum(abs(a - b) for a, b in zip(list1, list2))
 print("Part 1:", total)
 
 ### Part 2 ###
+counts_in_list2 = {}
+for x in list2:
+    counts_in_list2[x] = counts_in_list2.get(x, 0) + 1
+
 total = 0
 cache = {}
 
 for number in list1:
     if number in cache:
         total += cache[number]
-    else:
-        counts = 0
-        for x in list2:
-            if x == number:
-                counts += 1
-        if counts > 0: 
-            cache[number] = number*counts
-            total += cache[number]
+        continue
+        
+    if number in counts_in_list2:
+        cache[number] = number * counts_in_list2[number]
+        total += cache[number]
 
 print("Part 2:", total)
